f50833c06e44a931afac6057ac783978e2a310cc
George Sykes <gsykes537@gmail.com>
2023-06-19 22:57:35 +0100

    add warning

--- app/streamlit_app.py
+++ app/streamlit_app.py
@@ -2,20 +2,21 @@ import streamlit as st
 from streamlit_chat import message
 from PIL import Image
 from query.vortex_query import VortexQuery
-
+import json
 
 def initialize_page():
     st.set_page_config(page_title='Gladstone GPT', page_icon=':books:')
     st.image(logo_image, width=80)
     st.header("Gladstone GPT")
     st.write("This is a AI Chatbot with access to the Liberal Democray 2019 manifesto and articles from the party website. You can ask the chatbot any question and it will try to find an answer within the provided documentation. It can't generate new policy and can't argue in favour of policy only state in a concise manor.")
+    st.markdown("If the AI produces any content that is incorrect, offence or harmful please contact me on [email](mailto:gsykes537@gmail.com)")
     st.markdown("[Github](https://github.com/Tasty213/gladstone-gpt)")
 
 
 def handle_query_form():
     with st.form(key='query_form'):
-        user_query = st.text_input('Ask Gladstone a question on: ', '', key='input',
-                                   help='What do you want to know about lib dem policy?')
+        user_query = st.text_input('Ask Gladstone:', '', key='input',
+                                   help='Enter a question about Lib Dem policy.')
         submit_button = st.form_submit_button('Submit')
     return user_query, submit_button
 
@@ -34,8 +35,8 @@ def query(question: str) -> str:
     :return: The answer from the model
     """
     vortex_query = VortexQuery()
-    answer, _ = vortex_query.ask_question(question)
-    return answer
+    answer, source = vortex_query.ask_question(question)
+    return answer, source
 
 
 logo_image = Image.open('./logo.png')
@@ -48,8 +49,8 @@ initialize_page()
 user_query, submit_button = handle_query_form()
 
 if submit_button and user_query:
-    model_response = query(user_query)
+    answer, source = query(user_query)
     st.session_state.past.append(user_query)
-    st.session_state.generated.append(model_response)
+    st.session_state.generated.append(answer)
 
 display_chat_history()

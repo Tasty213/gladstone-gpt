f6266381fcde82bc8a611421c5c2cf203a0c7a86
George Sykes <gsykes537@gmail.com>
2023-09-02 19:07:49 +0100

    add database spans

--- app/backend/canvassData.py
+++ app/backend/canvassData.py
@@ -1,4 +1,6 @@
 import logging
+import os
+
 from botocore.exceptions import ClientError
 from opentelemetry import trace
 
@@ -15,28 +17,35 @@ class CanvassData:
         self.table = table
         self.logger = logging.getLogger()
 
-    @tracer.start_as_current_span("gladstone.CanvassData.add_canvass")
     def add_canvass(
         self, userId, firstName, lastName, postcode, email, voterIntent, time
     ):
-        try:
-            self.table.put_item(
-                Item={
-                    "userId": userId,
-                    "firstName": firstName,
-                    "lastName": lastName,
-                    "postcode": postcode,
-                    "email": email,
-                    "voterIntent": voterIntent,
-                    "time": time,
-                }
-            )
-        except ClientError as err:
-            self.logger.error(
-                "Couldn't add canvass %s to table %s. Here's why: %s: %s",
-                userId,
-                self.table.name,
-                err.response["Error"]["Code"],
-                err.response["Error"]["Message"],
-            )
-            raise
+        with tracer.start_as_current_span(
+            "gladstone.CanvassData.add_canvass",
+            attributes={
+                "db.system": "dynamodb",
+                "db.name": os.getenv("DB_NAME_CANVASS"),
+            },
+            kind=trace.SpanKind.CLIENT,
+        ):
+            try:
+                self.table.put_item(
+                    Item={
+                        "userId": userId,
+                        "firstName": firstName,
+                        "lastName": lastName,
+                        "postcode": postcode,
+                        "email": email,
+                        "voterIntent": voterIntent,
+                        "time": time,
+                    }
+                )
+            except ClientError as err:
+                self.logger.error(
+                    "Couldn't add canvass %s to table %s. Here's why: %s: %s",
+                    userId,
+                    self.table.name,
+                    err.response["Error"]["Code"],
+                    err.response["Error"]["Message"],
+                )
+                raise
--- app/backend/messageData.py
+++ app/backend/messageData.py
@@ -1,10 +1,13 @@
 import logging
+import os
+
 from botocore.exceptions import ClientError
 from opentelemetry import trace
 
 from schema.api_question import Message
 
 tracer = trace.get_tracer("gladstone.messageData")
+
 
 class MessageData:
     """Encapsulates an Amazon DynamoDB table of movie data."""
@@ -21,23 +24,31 @@ class MessageData:
         self,
         question: Message,
     ):
-        try:
-            self.table.put_item(
-                Item={
-                    "messageId": question.messageId,
-                    "userId": question.user,
-                    "message": question.message.content,
-                    "sources": question.sources,
-                    "time": question.time,
-                    "previousMessageId": question.previousMessageId,
-                }
-            )
-        except ClientError as err:
-            self.logger.error(
-                "Couldn't add canvass %s to table %s. Here's why: %s: %s",
-                question.user,
-                self.table.name,
-                err.response["Error"]["Code"],
-                err.response["Error"]["Message"],
-            )
-            raise
+        with tracer.start_as_current_span(
+            "gladstone.CanvassData.add_canvass",
+            attributes={
+                "db.system": "dynamodb",
+                "db.name": os.getenv("DB_NAME_MESSAGE"),
+            },
+            kind=trace.SpanKind.CLIENT,
+        ) as span:
+            try:
+                self.table.put_item(
+                    Item={
+                        "messageId": question.messageId,
+                        "userId": question.user,
+                        "message": question.message.content,
+                        "sources": question.sources,
+                        "time": question.time,
+                        "previousMessageId": question.previousMessageId,
+                    }
+                )
+            except ClientError as err:
+                self.logger.error(
+                    "Couldn't add canvass %s to table %s. Here's why: %s: %s",
+                    question.user,
+                    self.table.name,
+                    err.response["Error"]["Code"],
+                    err.response["Error"]["Message"],
+                )
+                raise

4416e5ec498a215a03017c2762638b614aceab45
George Sykes <gsykes537@gmail.com>
2023-09-30 23:29:38 +0100

    point get postcode tests to the correct files

--- app/backend/tests/test_get_local_party_details.py
+++ app/backend/tests/test_get_local_party_details.py
@@ -4,7 +4,7 @@ import pytest
 
 @pytest.mark.parametrize("postcode", ["ls121bu", "ls12 1bu", "default"])
 def test_valid_party(postcode):
-    with open(f"./data/{postcode}.txt", 'r', encoding="utf-8") as file:
+    with open(f"./tests/data/{postcode}.txt", 'r', encoding="utf-8") as file:
         expected = file.read()
 
     result = get_local_party_details(postcode)
